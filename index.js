edcdbb4d7c79ce3624e81da0d0b227c72881cc9d
Rewa14 <jlujan.sh@mysummitps.org>
2015-06-17 00:08:43 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+function Home() {
+    window.location.href = 'index.html';
+}
+
+function Vehicles() {
+    window.location.href = 'Aspect1.html';
+}
+
+function Computers() {
+    window.location.href = 'LiveAction.html';
+}
+
+function Pixar() {
+    window.location.href = 'Pixar.html';
+}
+
+function Campus() {
+    window.location.href = 'ThemeParks.html';
+}
+
+function toggleMe(a) {
+    var e = document.getElementById(a);
+    if(!e) return true;
+    if(e.style.display == "none") {
+        e.style.display = "block";
+    } else {
+        e.style.display = "none";
+    }
+    return false;
+}
+
+var index=0;
+    function changeBanner(){ 
+      [].forEach.call(document.getElementsByClassName("galleryImages"),function (v,i) { document.getElementsByClassName("galleryImages")[i].hidden = i!==index});
+      index = (index+1) % document.getElementsByClassName("galleryImages").length;
+    }
+    window.onload = function () {setInterval(changeBanner, 2000)};
