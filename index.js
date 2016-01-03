ce5f921719f7a48a6bedb4cb1c800c5306c4e3e9
Rewa14 <jlujan.sh@mysummitps.org>
2016-01-03 23:26:04 +0000

    everything, no blue borders and new logo

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//All
 function Home() {
     window.location.href = 'index.html';
 }
@@ -7,7 +8,7 @@ function Vehicles() {
 }
 
 function Computers() {
-    window.location.href = 'LiveAction.html';
+    window.location.href = 'Aspect2.html';
 }
 
 function Pixar() {
@@ -18,6 +19,40 @@ function Campus() {
     window.location.href = 'ThemeParks.html';
 }
 
+//Home
+loadBanner();
+
+var index=0;
+    function changeBanner(){ 
+      [].forEach.call(document.getElementsByClassName("galleryImages"),function (v,i) { document.getElementsByClassName("galleryImages")[i].hidden = i!==index});
+      index = (index+1) % document.getElementsByClassName("galleryImages").length;
+      console.log("test");    
+    }
+
+function loadBanner() {
+    var srcs = ["Images/P06.Campus1.jpg","Images/P07.Campus2.jpg","Images/P08.Campus3.jpg"];
+    var c = 0;
+    
+    imgs = [];
+    
+    for(i = srcs.length - 1; i >= 0; i--) {
+        imgs[i] = new Image();
+        imgs[i].src = srcs[i];
+        imgs[i].onload = function() {loaded();};
+    }
+    
+    loaded = function() {
+        c++;
+        if(c === srcs.length){
+            window.onload = function () {setInterval(changeBanner, 3000)};
+            changeBanner();
+        }
+    };
+    
+    this.update = function() {};
+}
+
+//Vehicles
 function toggleMe(a) {
     var e = document.getElementById(a);
     if(!e) return true;
@@ -28,11 +63,3 @@ function toggleMe(a) {
     }
     return false;
 }
-
-var index=0;
-    function changeBanner(){ 
-      [].forEach.call(document.getElementsByClassName("galleryImages"),function (v,i) { document.getElementsByClassName("galleryImages")[i].hidden = i!==index});
-      index = (index+1) % document.getElementsByClassName("galleryImages").length;
-    }
-    window.onload = function () {setInterval(changeBanner, 3000)};
-
